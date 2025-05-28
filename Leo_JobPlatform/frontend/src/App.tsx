da4d65b753f7f973c19918b87c0e36fc9079de02
Ylieo816 <leoyao816@gmail.com>
2025-05-29 05:33:47 +0800

    Create frontend basic and docker compose

---
+++ Leo_JobPlatform/frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Job Platform</h1>
+        <p>Find your next opportunity</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
